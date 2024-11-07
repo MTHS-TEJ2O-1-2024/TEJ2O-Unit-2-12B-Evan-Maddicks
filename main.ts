db3f4320b053f9c878bf884fa1b8c4e55721f8bf
Evan-Maddicks <evan.maddicks@mths.ca>
2024-11-07 16:20:53 -0500

    This program will detect objects within 10cm

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Oct 2024
+ * This program will detect objects within 10cm
 */
 
-basic.showString('Hello, World!')
+//variables
+let neopixelStrip: neopixel.Strip = null
+let distanceFromObject = PingUnit.Centimeters
+
+//cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//neopixels
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+//setup
+input.onButtonPressed(Button.A,function() {
+    basic.clearScreen()
+    neopixelStrip.clear()
+    distanceFromObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+if (distanceFromObject > 10) {
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    } else {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    }
+})
